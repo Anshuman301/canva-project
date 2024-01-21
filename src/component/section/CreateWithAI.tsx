6e3e4af8e211496ce35aedf590389da7456d7964
Anshuman301 <anshumanprsd@gmail.com>
2024-01-22 02:25:39 +0530

    resolved type error

--- src/component/section/CreateWithAI.tsx
+++ src/component/section/CreateWithAI.tsx
@@ -7,12 +7,12 @@ const image = faker.image.urlLoremFlickr({
 export default function CreateWithAI() {
     return (
         <HStack flexWrap={'wrap'} alignItems={'center'} px={'30px'} justify={'space-between'} py={'30px'}>
-            <VStack width={['100%',, '40%']}>
+            <VStack width={['100%',"100%", '40%']}>
                 <Text textStyle={'h2'} alignSelf={'flex-start'}>Create with AI</Text>
                 <Text textStyle={'p'} >Redefine the way you create with Magic Studio, our suite of AI-powered tools. Generate copy in your brand voice with </Text>
                 <Button>Discover Now</Button>
             </VStack>
-            <Box width={['100%',, '50%']}>
+            <Box width={['100%',"100%", '50%']}>
                 <Image src={image} alt='image' borderRadius={'10px'} />
             </Box>
         </HStack>
