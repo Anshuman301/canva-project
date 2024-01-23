c4c9d875db6f45924ff99c156eac395b0bc35730
Anshuman <anshuman@Anshumans-MacBook-Pro.local>
2024-01-23 17:39:34 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,45 @@ import {VitePWA} from 'vite-plugin-pwa'
 export default defineConfig({
   base: '/canva-project/',
   plugins: [react(),
-  VitePWA({registerType: 'autoUpdate', devOptions: {
-    enabled: true
-  }})],
+  VitePWA({
+    registerType: 'autoUpdate', 
+    devOptions: {
+    enabled:true
+  },
+  includeAssets: ['apple-touch-icon.png', 'favicon.ico', 'favicon.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        "icons": [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+      }
+  })],
   server: {
     port: 9000,
     open: true
